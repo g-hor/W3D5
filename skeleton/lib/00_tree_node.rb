9a31f58ed2fab3de9e5f68cc53e2ab01f8564c17
Gary <geehor@gmail.com>
2023-01-27 13:59:51 -0500

    Add Phase 1

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,28 @@
 class PolyTreeNode
 
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent
+    @parent
+  end
+
+  def children
+    @children
+  end
+
+  def value
+    @value
+  end
+
+  def parent=(par)
+    @parent = par
+    if par != nil
+      par.children << self
+    end
+  end
+
 end
